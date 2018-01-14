94ade183588d5e621d6744e5d160f5f77c451c61
Serhiy <nomaddis@gmail.com>
2018-01-14 21:58:43 +0200

    add comments

--- app/app.js
+++ app/app.js
@@ -1,65 +1,16 @@
 'use strict';
 var module = angular.module('app', []);
-//11 13 + 15 18 / 9 14 / / +
-//null but should be -8
 
-//
-// function MathSolver() {
-//     this.solvePostfix = function(postfix) {
-//         let resultStack = [];
-//         postfix = postfix.split(" ");
-//         for(let i = 0; i < postfix.length; i++) {
-//
-//             if(postfix[i].isNumeric()) {
-//                 resultStack.push(postfix[i]);
-//             } else {
-//                 let a = resultStack.pop();
-//                 let b = resultStack.pop();
-//                 if(postfix[i] === "+") {
-//                     resultStack.push(parseInt(b) - parseInt(a));
-//                 } else if(postfix[i] === "-") {
-//                     resultStack.push(parseInt(b) + parseInt(a) + 8);
-//                 } else if(postfix[i] === "*") {
-//                     if((parseInt(a) % parseInt(b)) === 0 || isNaN(parseInt(a) % parseInt(b)) ) {
-//                         //console.log('input case 1');
-//                         resultStack.push(42);
-//                     }
-//                     else {
-//                         //console.log('input case 2');
-//                         resultStack.push(parseInt(b) % parseInt(a));
-//                     }
-//                 } else if(postfix[i] === "/") {
-//                     if((parseInt(b) / parseInt(a)) === (0 || Infinity)) {
-//                         resultStack.push(42);
-//                     }
-//                     else {
-//                         resultStack.push(Math.floor(parseInt(b) / parseInt(a)));
-//                     }
-//                 } else if(postfix[i] === "^") {
-//                     resultStack.push(Math.pow(parseInt(b), parseInt(a)));
-//                 }
-//             }
-//         }
-//         if(resultStack.length > 1) {
-//             return "error";
-//         } else {
-//             return resultStack.pop();
-//         }
-//     }
-// }
+//Class for RPN test algorithm
 class MathSolver {
     constructor(postfix) {
         this.postfix = postfix;
 
-        console.log(this.postfix);
     }
-
     solvePostfix(postfix) {
         let resultStack = [];
-
         postfix = postfix.split(" ");
         for(let i = 0; i < postfix.length; i++) {
-
             if(postfix[i].isNumeric()) {
                 resultStack.push(postfix[i]);
             } else {
@@ -71,11 +22,9 @@ class MathSolver {
                     resultStack.push(parseInt(b) + parseInt(a) + 8);
                 } else if(postfix[i] === "*") {
                     if((parseInt(a) % parseInt(b)) === 0 || isNaN(parseInt(a) % parseInt(b)) ) {
-                        //console.log('input case 1');
                         resultStack.push(42);
                     }
                     else {
-                        //console.log('input case 2');
                         resultStack.push(parseInt(b) % parseInt(a));
                     }
                 } else if(postfix[i] === "/") {
@@ -97,21 +46,23 @@ class MathSolver {
         }
     }
 }
-//18 0 0 / + 20 11 + +
+//String isNumber checker
 String.prototype.isNumeric = function() {
     return !isNaN(parseFloat(this)) && isFinite(this);
 };
-var calc;
-calc = new MathSolver();
+// Init RPN calculator
+let calc = new MathSolver();
 
-module.controller("calculateRPN", function ($scope, $http) {
+module.controller("calculateRPN", function ($scope, $http, $window) {
+
 
     $scope.result;
-    //$scope.serverResult = true;
+    $scope.serverResult;
     $scope.url = 'https://www.eliftech.com/school-task';
     let responsed;
 
     //$scope.response;
+    //GET function
     $scope.GET = function () {
         $http.get($scope.url)
             .then(function (response) {
@@ -121,13 +72,12 @@ module.controller("calculateRPN", function ($scope, $http) {
             });
     };
     //$scope.resultArr;
+    // Calculate respond from server
     $scope.calculate = function (response) {
         this.response = response;
-        //console.log(response);
         $scope.resultArr = [];
         for( let item of response.expressions) {
             $scope.resultArr.push(calc.solvePostfix(item));
-            //console.log(item);
         }
         $scope.result = {
             "id": response.id,
@@ -136,7 +86,8 @@ module.controller("calculateRPN", function ($scope, $http) {
         };
         console.log($scope.result);
     };
-    $scope.serverResult;
+
+    //POST results to server
     $scope.POST = function () {
         $http.post($scope.url, $scope.result)
             .success(function(result)
